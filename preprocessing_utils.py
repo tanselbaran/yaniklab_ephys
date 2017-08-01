8e571f12e6fbb3be6c2921c42337ff97d6d34d97
tanselbaran <tanselbaranyasar@gmail.com>
2017-08-01 16:38:00 +0200

    Changes in the preprocessing_utils.py Added the overall documentation, and documentations for the individual functions. Added the default parameters for SpikeDetekt and Klustakwik to be written in the prm files. Added the probe index as an input to the tetrode processing functions. Updated the package information in the import section of the code.

--- preprocessing_utils.py
+++ preprocessing_utils.py
@@ -1,3 +1,11 @@
+"""
+Creation date: Tuesday, Aug 1st 2017
+
+authors: Tansel Baran Yasar and Clemens Dlaska
+
+Contains the functions that are used for creating the .prm and .prb files required for spike-sorting with Klustakwik, and running the Klustakwik on the tetrode or shank (for linear probes). 
+"""
+
 import numpy as np
 import os as os
 import h5py
@@ -6,134 +14,115 @@ import json
 import pickle
 from scipy import signal
 from shutil import copyfile
+from filtering import *
 
+### Klustakwik utilities for tetrode data analyzing ###
 
-def create_tetrode_prm_file(h,s,p):
-   file_dir = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)+'/tetrode_{:g}_{:g}.prm'.format(h,s)
-   with open(file_dir, 'a') as text:
+def create_tetrode_prm_file(probe,h,s,p):
+    """
+    This function creates the .prm file required by Klustakwik for the analysis of the data from the tetrode located at shank s and height h.
+
+    Inputs:
+        probe: Index of the probe, for the recordings with multiple probes
+        h: Height index of the tetrode (0 for the bottom-most tetrode)
+        s: Shank index of the tetrode (0 for the left-most shank, looking at the electrode side)
+        p: Parameters dictionary containing the parameters and preferences related to spike sorting.
+    """
+
+    file_dir = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)+'/tetrode_{:g}_{:g}.prm'.format(h,s)
+    with open(file_dir, 'a') as text:
         print('experiment_name = \'tetrode_{:g}_{:g}\''.format(h,s), file = text)
         print('prb_file = \'tetrode_{:g}_{:g}.prb\''.format(h,s), file = text)
         print('traces = dict(raw_data_files=[experiment_name + \'.dat\'],voltage_gain=10., sample_rate =' + str(p['sample_rate']) + ', n_channels = 4, dtype = \'int16\')', file = text)
-        print('spikedetekt = dict()', file = text)
-        print('klustakwik2 = dict(num_starting_clusters=100,)', file = text)       
 
+        print("spikedetekt = 'filter_low' : 500., \n 'filter_high_factor': 0.95 * .5, \n 'filter_butter_order': 3, \n #Data chunks. \n 'chunk_size_seconds': 1., \n 'chunk_overlap_seconds': .015, \n #Threshold \n 'n_excerpts': 50, \n 'excerpt_size_seconds': 1., \n 'use_single_threshold': True, \n 'threshold_strong_std_factor': 4.5 \n 'threshold_weak_std_factor': 2., \n 'detect_spikes': 'negative', \n # Connected components. \n 'connected_component_join_size': 1, \n #Spike extractions. \n 'extract_s_before': 10, \n 'extract_s_after': 10, \n 'weight_power': 2, \n #Features. \n 'n_features_per_channel': 3, \n 'pca_n_waveforms_max': 10000}", file = text)
 
-def create_tetrode_prb_file(h,s,p):
+        print('klustakwik2 = dict(prior_point=1, \n mua_point=2, \n noise_point=1, \n points_for_cluster_mask=100, \n penalty_k=0.0, \n penalty_k_log_n=1.0, \n max_iterations=1000, \n num_starting_clusters=500, \n use_noise_clusters=True, \n use_mua_cluster=True, \n num_changed_threshold=0.05, \n full_step_every=1, \n split_first=20, \n split_every=40, \n max_possible_clusters=1000, \n dist_thresh=log(10000.0), \n max_quick_step_candidates=100000000, \n max_quick_step_candidates_fraction=0.4, \n always_split_bimodal=False, \n subset_break_fraction=0.01, \n break_fraction=0.0, \n fast_split=False, \n max_split_iterations=None, \n consider_cluster_deletion=True, \n num_cpus=None)', file = text)
+
+    text.close()
+
+def create_tetrode_prb_file(probe,h,s,p):
+    """
+    This function creates the .prb file required by Klustakwik, which contains the electrode neighborhood and geometry information for the tetrode located at shank s and height h.
+
+    Inputs:
+        probe: Index of the probe, for the recordings with multiple probes
+        h: Height index of the tetrode (0 for the bottom-most tetrode)
+        s: Shank index of the tetrode (0 for the left-most shank, looking at the electrode side)
+        p: Parameters dictionary containing the parameters and preferences related to spike sorting.
+    """
+
     file_dir = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)+'/tetrode_{:g}_{:g}.prb'.format(h,s)
     tetrode = dict(channels = list(range(4)), graph = list(combinations(range(4),2)), geometry = {0: (0, 90), 1: (0, 60), 2: (0, 30), 3: (0, 0)})
     channel_groups = {0: tetrode}
     with open(file_dir, 'a') as text:
-        print('channel_groups = {}'.format(channel_groups), file = text)    
-        
-def do_klusta(h,s,p):
+        print('channel_groups = {}'.format(channel_groups), file = text)
+
+def do_klusta_for_tetrode(probe,h,s,p):
+    """
+    This function runs the klustakwik for the tetrode.
+
+    Inputs:
+        probe: Index of the probe, for the recordings with multiple probes
+        h: Height index of the tetrode (0 for the bottom-most tetrode)
+        s: Shank index of the tetrode (0 for the left-most shank, looking at the electrode side)
+        p: Parameters dictionary containing the parameters and preferences related to spike sorting.
+    """
+
     file_dir = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)
-    os.chdir(file_dir) 
+    os.chdir(file_dir)
     os.system('klusta tetrode_{:g}_{:g}.prm'.format(h,s))
 
+### Klustakwik utilities for linear probe data analyzing ###
+
 def create_shank_prm_file(probe,s,p):
+    """
+    This function creates the .prm file required by Klustakwik for the analysis of the data from the tetrode located at shank s and height h.
+
+    Inputs:
+        probe: Index of the probe, for recordings with multiple probes.
+        s: Shank index (0 for the left-most shank, looking at the electrode side)
+        p: Parameters dictionary containing the parameters and preferences related to spike sorting.
+    """
+
     file_dir = p['path'] + '/probe_{:g}_shank_{:g}'.format(probe,s) + '/probe_{:g}_shank_{:g}.prm'.format(probe,s)
     if os.path.exists(file_dir):
         os.remove(file_dir)
+
     with open(file_dir, 'a') as text:
         print('experiment_name = \'probe_{:g}_shank_{:g}\''.format(probe,s), file = text)
         print('prb_file = \'probe_{:g}_shank_{:g}.prb\''.format(probe,s), file = text)
         print('traces = dict(raw_data_files=[experiment_name + \'.dat\'],voltage_gain=10., sample_rate =' + str(p['sample_rate']) + ', n_channels = ' + str(p['nr_of_electrodes']) + ', dtype = \'int16\')', file = text)
-        print('spikedetekt = dict()', file = text)
-        print('klustakwik2 = dict(num_starting_clusters=100,)', file = text)  
+
+        print("spikedetekt = 'filter_low' : 500., \n 'filter_high_factor': 0.95 * .5, \n 'filter_butter_order': 3, \n #Data chunks. \n 'chunk_size_seconds': 1., \n 'chunk_overlap_seconds': .015, \n #Threshold \n 'n_excerpts': 50, \n 'excerpt_size_seconds': 1., \n 'use_single_threshold': True, \n 'threshold_strong_std_factor': 4.5 \n 'threshold_weak_std_factor': 2., \n 'detect_spikes': 'negative', \n # Connected components. \n 'connected_component_join_size': 1, \n #Spike extractions. \n 'extract_s_before': 10, \n 'extract_s_after': 10, \n 'weight_power': 2, \n #Features. \n 'n_features_per_channel': 3, \n 'pca_n_waveforms_max': 10000}", file = text)
+
+        print('klustakwik2 = dict(prior_point=1, \n mua_point=2, \n noise_point=1, \n points_for_cluster_mask=100, \n penalty_k=0.0, \n penalty_k_log_n=1.0, \n max_iterations=1000, \n num_starting_clusters=500, \n use_noise_clusters=True, \n use_mua_cluster=True, \n num_changed_threshold=0.05, \n full_step_every=1, \n split_first=20, \n split_every=40, \n max_possible_clusters=1000, \n dist_thresh=log(10000.0), \n max_quick_step_candidates=100000000, \n max_quick_step_candidates_fraction=0.4, \n always_split_bimodal=False, \n subset_break_fraction=0.01, \n break_fraction=0.0, \n fast_split=False, \n max_split_iterations=None, \n consider_cluster_deletion=True, \n num_cpus=None)', file = text)
+
     text.close()
-        
+
 def create_shank_prb_file(probe,s,p):
+    """
+    This function copies the .prb file required by Klustakwik for the probe used in recording from the prb_files folder in the repository.
+
+    Inputs:
+        probe: Index of the probe, for recordings with multiple probes.
+        s: Shank index (0 for the left-most shank, looking at the electrode side)
+        p: Parameters dictionary containing the parameters and preferences related to spike sorting.
+    """
+
     file_dir = p['path'] + '/probe_{:g}_shank_{:g}'.format(probe,s) + '/probe_{:g}_shank_{:g}.prb'.format(probe,s)
     copyfile('/home/baran/Desktop/tetrode_pipeline/prb_files/' + p['probe_name'] + '.prb', file_dir)
-    
+
 def do_klusta_for_shank(probe,s,p):
+    """
+    This function runs the klustakwik for the shank. It creates a new folder for the shank first.
+
+    Inputs:
+        probe: Index of the probe, for recordings with multiple probes.
+        s: Shank index (0 for the left-most shank, looking at the electrode side)
+        p: Parameters dictionary containing the parameters and preferences related to spike sorting.
+    """
+
     file_dir = p['path'] + '/probe_{:g}_shank_{:g}'.format(probe,s)
-    os.chdir(file_dir) 
+    os.chdir(file_dir)
     os.system('klusta probe_{:g}_shank_{:g}.prm'.format(probe,s))
-    
-def timestamps_isi_waveforms(h,s,c,p):
-    path_kwik_file = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)+ '/tetrode_{:g}_{:g}.kwik'.format(h,s)
-    with h5py.File(path_kwik_file,'r') as hf:
-        all_spiketimes = hf.get('channel_groups/0/spikes/time_samples')
-        np_all_spiketimes = np.array(all_spiketimes)
-        
-    path_clu_file = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)+ '/tetrode_{:g}_{:g}.clu.0'.format(h,s)
-    np_clu_original = np.loadtxt(path_clu_file)
-    np_clu = np_clu_original[1:]
-    spike_times_cluster_index = np.where(np_clu == c)
-    spike_times_cluster = np_all_spiketimes[spike_times_cluster_index]
-    
-    isi = [spike_times_cluster[i+1]-spike_times_cluster[i] for i in range(len(spike_times_cluster)-1)]
-    
-    raw_data = np.fromfile(p['path'] + '/tetrode_{:g}_{:g}'.format(h,s) + '/tetrode_{:g}_{:g}.dat'.format(h,s) ,dtype='int16')
-    fil = Filter(rate=20000., low=500., high=6000., order=3) 
-    raw_data_f = fil(raw_data)
-    N = len(spike_times_cluster)
-    M = p['samples_per_spike']
-    waveforms = np.zeros((N,4,M))
-    waveforms_f = np.zeros((N,4,M))
-    for i in range(N):
-        for j in range(4):
-            for k in range(M):
-                waveforms[i,j,k] = raw_data[(int(spike_times_cluster[i])-p['samples_before']+k)*4+j]
-                waveforms_f[i,j,k] = raw_data_f[(int(spike_times_cluster[i])-p['samples_before']+k)*4+j]
-        
-    D = [0,0,0,0]
-    D[0] = spike_times_cluster
-    D[1] = isi
-    D[2] = waveforms
-    D[3] = waveforms_f
-    
-
-    
-    path_pickle_file = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)+ '/tetrode_{:g}_{:g}_c{:g}_timestamps_isi_waveform.pickle'.format(h,s,c)
-    with open (path_pickle_file, 'wb') as f:
-        pickle.dump(D,f)
-        
-
-def generate_spikeinfo_pickle(h,s,p):
-    path_kwik_file = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)+ '/tetrode_{:g}_{:g}.kwik'.format(h,s)
-    with h5py.File(path_kwik_file,'r') as hf:
-        all_spiketimes = hf.get('channel_groups/0/spikes/time_samples')
-        np_all_spiketimes = np.array(all_spiketimes)
-        
-    path_clu_file = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)+ '/tetrode_{:g}_{:g}.clu.0'.format(h,s)
-    np_clu_original = np.loadtxt(path_clu_file)
-    nr_of_clusters = int(np_clu_original[0])
-    np_clu = np_clu_original[1:]
-    
-    raw_data = np.fromfile(p['path'] + '/tetrode_{:g}_{:g}'.format(h,s) + '/tetrode_{:g}_{:g}.dat'.format(h,s) ,dtype='int16')
-    fil = Filter(rate=20000., low=500., high=6000., order=3) 
-    raw_data_f = fil(raw_data)
-    
-    P={}
-    
-    for l in range(nr_of_clusters+1):
-        spike_times_cluster_index = np.where(np_clu == l)
-        spike_times_cluster = np_all_spiketimes[spike_times_cluster_index]
-        isi = [spike_times_cluster[i+1]-spike_times_cluster[i] for i in range(len(spike_times_cluster)-1)]
-        N = len(spike_times_cluster)
-        M = p['samples_per_spike']
-        waveforms = np.zeros((N,4,M))
-        waveforms_f = np.zeros((N,4,M))
-        for i in range(N):
-            for j in range(4):
-                for k in range(M):
-                    waveforms[i,j,k] = raw_data[(int(spike_times_cluster[i])-p['samples_before']+k)*4+j]
-                    waveforms_f[i,j,k] = raw_data_f[(int(spike_times_cluster[i])-p['samples_before']+k)*4+j]
-            
-        D = [0,0,0,0]
-        D[0] = spike_times_cluster
-        D[1] = isi
-        D[2] = waveforms
-        D[3] = waveforms_f
-        
-        P['c{:g}'.format(l)] = D
-        
-        data = {'raw_data':raw_data, 'p':p}
-    path_pickle_file = p['path'] + '/tetrode_{:g}_{:g}'.format(h,s)+ '/tetrode_{:g}_{:g}_spikeinfo.pickle'.format(h,s)
-    with open (path_pickle_file, 'wb') as f:
-        pickle.dump({'P':P, 'data':data} ,f)
-     
-    
-       
