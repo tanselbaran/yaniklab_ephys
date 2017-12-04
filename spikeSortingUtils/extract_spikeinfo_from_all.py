0120ab048a17fc9d20ec1e7e2d85f1fadb511ecd
tanselbaran <tanselbaranyasar@gmail.com>
2017-12-04 10:32:44 +0100

    Changed the script to run only on the common folder for all sessions

--- spikeSortingUtils/extract_spikeinfo_from_all.py
+++ spikeSortingUtils/extract_spikeinfo_from_all.py
@@ -17,7 +17,7 @@ import sys
 from spike_postprocessing_utils import *
 
 mainPath = sys.stdin.read().splitlines()[0] #Reads the mainPath from bash input.
-dirs = os.listdir(mainPath)
+"""dirs = os.listdir(mainPath)
 
 for folder in (folder for folder in dirs if ((folder != 'log.txt') and (folder != 'notes.docx') and (folder != 'analyzed') and (folder != 'other'))):
     #Iterating over the folders for individual recording sessions, while skipping other files and folders.
@@ -27,4 +27,10 @@ for folder in (folder for folder in dirs if ((folder != 'log.txt') and (folder !
 
     for probe in range(p['probes']):
         for group in range(p['nr_of_groups']):
-            retain_cluster_info(probe,group,p)                  
+            retain_cluster_info(probe,group,p)"""
+
+folder = 'analysis_files'
+p =  pickle.load(open(mainPath + folder + '/paramsDict.p', 'rb'))
+for probe in range(p['probes']):
+    for group in range(p['nr_of_groups']):
+        retain_cluster_info(probe,group,p)
