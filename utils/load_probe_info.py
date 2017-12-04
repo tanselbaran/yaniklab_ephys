faf05602d5b8455e9501f768ac81e9335040eb4f
shadowfax <aagamshah@public-docking-als-1591.ethz.ch>
2017-12-04 15:20:41 +0100

    adding more probes

--- utils/load_probe_info.py
+++ utils/load_probe_info.py
@@ -72,4 +72,37 @@ def load_probe_info(probe):
 
         probe_info['id'] = id
 
+    elif probe == 'a3x8_5mm_100_200_177':
+        probe_info['numShanks'] = 3
+        probe_info['type'] = 'linear'
+        probe_info['numTrodesPerShank'] = 8
+        probe_info['numTrodes'] = 24
+
+        probe_info['bottom_ycoord'] = 0
+        probe_info['top_ycoord'] = 800
+
+        ###Channel mapping###
+        neuronexus_to_intan = [30,26,21,17,27,22,20,25,28,23,19,24,29,18,31,16,0,15,2,13,8,9,7,1,6,14,10,11,5,12,4,3]
+        id = np.zeros((probe_info['numTrodesPerShank'], probe_info['numShanks'],))
+        for i in range(probe_info['numShanks']):
+            id[:,i] = [neuronexus_to_intan[0+i*8], neuronexus_to_intan[7+i*8], neuronexus_to_intan[1+i*8], neuronexus_to_intan[6+i*8], neuronexus_to_intan[2+i*8], neuronexus_to_intan[5+i*8], neuronexus_to_intan[3+i*8], neuronexus_to_intan[4+i*8]]
+        probe_info['id'] = id
+
+        #2X16 linear probe  a2x16_10mm_100_500_177
+    elif probe == 'a2x16_10mm_100_500_177':
+        probe_info['numShanks'] = 2
+        probe_info['type'] = 'linear'
+        probe_info['numTrodesPerShank'] = 16
+        probe_info['numTrodes'] = 32
+
+        probe_info['bottom_ycoord'] = 0
+        probe_info['top_ycoord'] = 1600
+
+        ###Channel mapping###
+        neuronexus_to_intan = [30,26,21,17,27,22,20,25,28,23,19,24,29,18,31,16,0,15,2,13,8,9,7,1,6,14,10,11,5,12,4,3]
+        id = np.zeros((probe_info['numTrodesPerShank'], probe_info['numShanks'],))
+        for i in range(probe_info['numShanks']):
+            id[:,i] = [neuronexus_to_intan[0+i*16], neuronexus_to_intan[15+i*16], neuronexus_to_intan[1+i*16], neuronexus_to_intan[14+i*16], neuronexus_to_intan[2+i*16], neuronexus_to_intan[13+i*16], neuronexus_to_intan[3+i*16], neuronexus_to_intan[12+i*16], neuronexus_to_intan[4+i*16], neuronexus_to_intan[11+i*16], neuronexus_to_intan[5+i*16], neuronexus_to_intan[10+i*16], neuronexus_to_intan[6+i*16], neuronexus_to_intan[9+i*16], neuronexus_to_intan[7+i*16], neuronexus_to_intan[8+i*16]]
+        probe_info['id'] = id
+
     return probe_info
