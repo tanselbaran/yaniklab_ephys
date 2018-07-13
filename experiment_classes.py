a0d9544832321cc916e8edc6bd8226def5568062
tanselbaran <tanselbaranyasar@gmail.com>
2018-07-13 20:16:49 +0200

    Added session orders and fixed bugs in experiment_classes

--- experiment_classes.py
+++ experiment_classes.py
@@ -16,6 +16,15 @@ class Experiment:
         else:
             raise ValueError('Invalid probe type.')
 
+    def get_input_for_pref(self,statement):
+        while True:
+            inpt = input(statement)
+            if (inpt == 'y' or inpt == 'n'):
+                break
+            else:
+                print("Invalid input! Please enter a valid input (y or n).")
+        return inpt
+
 
 class Session:
     def __init__(self, session_name, subExperiment):
@@ -38,7 +47,6 @@ class Session:
         preferences['do_whisker_stim_evoked'] = self.get_input_for_pref("Do whisker stimulation evoked analysis for this session? (y/n)")
         preferences['do_optical_stim_evoked'] = self.get_input_for_pref("Do optical stimulation evoked analysis for this session? (y/n)")
         preferences['do_electrical_stim_evoked'] = self.get_input_for_pref("Do electrical stimulation evoked analysis for this session? (y/n)")
-        preferences["do_spike_analysis"] = self.get_input_for_pref("Do spike detection, sorting and post-processing for this session? (y/n)")
         preferences["do_spectogram_analysis"] = self.get_input_for_pref("Do spectogram analysis on low frequency LFP for this session? (y/n)")
         self.preferences = preferences
 
@@ -56,7 +64,7 @@ class Session:
         if self.preferences['do_whisker_stim_evoked'] == 'y':
             self.whisker_stim_channel = self.dir + '/' + prefix + str(args[0]) + '.dat'
         if self.preferences['do_optical_stim_evoked'] == 'y':
-            self.whisker_stim_channel = self.dir + '/' + prefix + str(args[1]) + '.dat'
+            self.optical_stim_channel = self.dir + '/' + prefix + str(args[1]) + '.dat'
         if self.preferences['do_electrical_stim_evoked'] == 'y':
             #TO BE FILLED
             pass
@@ -70,6 +78,11 @@ class acute(Experiment):
     def add_location(self, location_dir):
         index_location = len(self.locations)
         self.locations[index_location] = Location(location_dir, self)
+        print(location_dir)
+        self.locations[index_location].amplifier_port = input('Please enter the amplifier port to which the amplifier is connected to')
+        preferences = {}
+        preferences['do_spike_analysis'] = self.get_input_for_pref("Do spike detection, sorting and post-processing for this session? (y/n)")
+        self.locations[index_location].preferences = preferences
 
 class subExperiment:
     def __init__(self, location_dir, experiment):
@@ -77,16 +90,28 @@ class subExperiment:
         self.sessions = {}
         self.experiment = experiment
 
-    def add_session(self, session_name):
+    def add_session(self, session_name, order):
         index_session = len(self.sessions)
         self.sessions[index_session] = Session(session_name, self)
         self.sessions[index_session].set_analysis_preferences()
+        self.sessions[index_session].order = order
 
     def add_sessions_in_dir(self):
-        subdirs = glob(self.dir + "/*")
+        subdirs = sorted(glob(self.dir + "/*"))
         for subdir in subdirs:
+            current_session = 1
             session_name = subdir.split('/')[-1]
-            self.add_session(session_name)
+            if len(subdirs) == 1:
+                order = 3
+            elif current_session == 1:
+                order = 0
+            elif current_session == len(subdirs):
+                order = 2
+            else:
+                order = 1
+            self.add_session(session_name, order)
+            current_session = current_session + 1
+
 
 class chronic(Experiment):
     def __init__(self, experiment_dir):
@@ -96,6 +121,7 @@ class chronic(Experiment):
     def add_day(self, day_dir):
         index_day = len(self.days)
         self.days[index_day] = Day(day_dir, self)
+        self.days[index_day].amplifier_port = input('Please enter the amplifier port to which the amplifier is connected to')
 
 class Location(subExperiment):
     pass
