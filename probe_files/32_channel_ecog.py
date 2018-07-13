e1ffc547dcb7b8c9797f44d5862bc700ee3002df
tanselbaran <tanselbaranyasar@gmail.com>
2018-07-13 20:17:20 +0200

    Fixed bug in 32_channel_ecog.py

--- probe_files/32_channel_ecog.py
+++ probe_files/32_channel_ecog.py
@@ -6,10 +6,10 @@ probe_info['nr_of_electrodes_per_group'] = 8
 probe_info['numTrodes'] = 32
 
 ###Channel mapping###
-id = np.zeros((probe_info['nr_of_groups'], probe_info['nr_of_electrodes_per_group']))
+id = np.zeros((probe_info['nr_of_electrodes_per_group'], probe_info['nr_of_groups']))
 electrode = 0
 for group in range(probe_info['nr_of_groups']):
     for channel in range(probe_info['nr_of_electrodes_per_group']):
-        id[group][channel] = electrode
+        id[channel][group] = electrode
         electrode = electrode + 1
 probe_info['id'] = id
